7fdf2493314b54e912e7e80155bebfb4b627ec67
RavenAV <anubis.27007@mail.ru>
2024-09-22 20:29:15 +0400

    Adding company service

---
+++ company-service/src/main/java/com/example/companyservice/config/ServiceDescriptionConfig.java
@@ -0,0 +1,12 @@
+package com.example.companyservice.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class ServiceDescriptionConfig {
+    @Value("${custom.property.description}")
+    private String description;
+}
---
+++ company-service/src/main/java/com/example/companyservice/controller/PropertiesController.java
@@ -0,0 +1,19 @@
+package com.example.companyservice.controller;
+
+import com.example.companyservice.config.ServiceDescriptionConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/properties")
+public class PropertiesController {
+    private final ServiceDescriptionConfig descriptionConfig;
+
+    @GetMapping("/description")
+    public String getServiceDescription() {
+        return descriptionConfig.getDescription();
+    }
+}
